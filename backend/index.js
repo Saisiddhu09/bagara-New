bf72c2e5975e829377ac0f55557293da025fbb0c
Idioticapricot <akarthikeyasmail@gmail.com>
2025-02-26 11:58:30 +0530

    #

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ mongoose
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000", // Allow frontend access
+    origin: process.env.FRONTEND_URL, // Allow frontend access
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"], // Ensure JWT can be sent in headers
   })
@@ -29,7 +29,7 @@ app.use(
 app.use("/api/forms", formRoutes);
 
 // **Start server**
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
