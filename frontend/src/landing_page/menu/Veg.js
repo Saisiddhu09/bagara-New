5e8f433827e6000822fe7433fee97eae340a833f
Idioticapricot <akarthikeyasmail@gmail.com>
2025-02-27 11:34:04 +0530

    #

--- frontend/src/landing_page/menu/Veg.js
+++ frontend/src/landing_page/menu/Veg.js
@@ -6,10 +6,10 @@ const Veg = () => {
       {/* Adding extra padding for navbar space */}
       <div className="row justify-content-center">
         <div className="col-md-4">
-          <img src={`${process.env.PUBLIC_URL}/media/images/veg1.jpg`} className="img-fluid" alt="" />
+          <img src={`${process.env.PUBLIC_URL}/media/images/veg1.JPG`} className="img-fluid" alt="" />
         </div>
         <div className="col-md-4">
-          <img src={`${process.env.PUBLIC_URL}/media/images/veg2.jpg`} className="img-fluid" alt="" />
+          <img src={`${process.env.PUBLIC_URL}/media/images/veg2.JPG`} className="img-fluid" alt="" />
         </div>
         <div className="col-md-4">
           <img src={`${process.env.PUBLIC_URL}/media/images/veg3.jpg`} className="img-fluid" alt="" />
